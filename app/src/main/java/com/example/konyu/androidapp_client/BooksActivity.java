673e2186e1762a108ad4e9a69503ddf2e3e2d619
xDanteGooDx <konyukov1997@inbox.ru>
2019-02-24 16:13:27 +0300

    Added ToolBar

--- app/src/main/java/com/example/konyu/androidapp_client/BooksActivity.java
+++ app/src/main/java/com/example/konyu/androidapp_client/BooksActivity.java
@@ -1,11 +1,16 @@
 package com.example.konyu.androidapp_client;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class BooksActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,19 @@ public class BooksActivity extends AppCompatActivity {
         //------
         Intent intent = getIntent();
         String Token = intent.getStringExtra(MainActivity.EXTRA_Token);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("IT-портал");
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
